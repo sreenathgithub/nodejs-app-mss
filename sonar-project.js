79b179baa417cc10c77fb1cb23180dc1c9fc0a4c
sreenathgithub <94750888+sreenathgithub@users.noreply.github.com>
2023-06-01 15:14:12 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://192.168.42.10:9000/',
+  serverUrl: 'http://192.168.158.10:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
